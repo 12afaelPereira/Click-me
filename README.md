7ad0583c7905ad7e6ba41ea46330d32266288a12
Rafael Pereira <rafaelpereira2.0@outlook.com>
2016-07-07 01:17:31 +0000

    modifield README, clickMe.html, clickMe.css and clickMe.js

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Click-me
 Change the background color clicking on the text.
 
-- [Lauch Project] (https://12afaelpereira.github.io/Click-me/)
+- [Demo] (https://12afaelpereira.github.io/Click-me/)
 
 
 ![alt tag](https://raw.githubusercontent.com/12afaelPereira/Click-me/master/img/click.png)
--- js/clickMe.js
+++ js/clickMe.js
@@ -2,16 +2,16 @@
 	var background;
 	var text;
 
-	var colors = ["#FF8F17", "#87CEEB", "#A9A9A9", "#C0C0C0", "#66CDAA", "#D3D3D3", "#FFFACD", "#EEE8AA", "#53DF83", "#66CC99", "#ADD8E6", "#5F9EA0", "#00BFFF", "#6495ED", "#FFD700", "#20B2AA", "#FC575E", "#55C34D"];
+	var colors = ["#FF8F17", "#87CEEB", "#A9A9A9", "#C0C0C0", "#66CDAA", "#D3D3D3", "#C0BA78", "#EEE8AA", "#53DF83", "#66CC99", "#ADD8E6", "#5F9EA0", "#00BFFF", "#6495ED", "#FFD700", "#20B2AA", "#FC575E", "#55C34D"];
 
 	function init(){
 		background = document.body;
 
 		text = document.getElementsByClassName("click-me");
 		text = text[0];
-
+		
 		text.style.cursor = "pointer";
-
+		
 		text.addEventListener("click", changeBgColor, false);
 	}
 
